d176569abb5e06a06ab1f016ad76272963d197ba
Official Bika <phoetaay@gmail.com>
2026-09-20 14:59:34 +0700

    feat: configure temporary giveaway data retention

--- src/config.js
+++ src/config.js
@@ -13,7 +13,9 @@ function loadConfig() {
     pickCountMax: intEnv('PICK_COUNT_MAX', 20, 1, 100), rollDurationSeconds: intEnv('ROLL_DURATION_SECONDS', 20, 0, 120),
     entryCooldownMs: intEnv('ENTRY_COOLDOWN_MS', 0, 0, 86400000), broadcastConcurrency: intEnv('BROADCAST_CONCURRENCY', 1, 1, 10),
     broadcastDelayMs: intEnv('BROADCAST_DELAY_MS', 80, 0, 10000), broadcastMaxRetries: intEnv('BROADCAST_MAX_RETRIES', 3, 0, 10), logLevel: process.env.LOG_LEVEL || 'info',
-    rollingEmojiId: process.env.ROLLING_EMOJI_ID || '5258077393884562645'
+    rollingEmojiId: process.env.ROLLING_EMOJI_ID || '5258077393884562645',
+    tempDataRetentionDays: intEnv('TEMP_DATA_RETENTION_DAYS', 90, 1, 3650),
+    cleanupIntervalMinutes: intEnv('CLEANUP_INTERVAL_MINUTES', 60, 5, 1440)
   });
 }
 module.exports = { loadConfig };
